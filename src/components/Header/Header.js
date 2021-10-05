c0fb7424d47cbdbf23da32ac41d6f400cd232e5e
prangancode <senprangan@gmail.com>
2021-10-05 18:42:05 +0600

    deploy successful

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div>
-            <nav className=" bg-indigo-700 flex items-center justify-between flex-wrap bg-teal-500 p-6">
-                <div className="flex items-center flex-shrink-0 text-white mr-6 ml-80">
+            <nav className=" bg-indigo-700 flex items-center justify-between flex-wrap bg-teal-500 p-5">
+                <div className="flex items-center flex-shrink-0 text-white mr-6 mx-8">
                     <svg className="animate-bounce-slow fill-current mr-2 w-1/3" width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /></svg>
                     <span className="font-serif lg:flex-grow text-2xl">EDU Assists</span>
                 </div>
@@ -16,16 +16,16 @@ const Header = () => {
                 </div>
                 <div className="w-full block flex-grow-0 lg:flex lg:items-center lg:w-auto">
                     <div className="font-serif lg:flex-grow text-2xl mr-40">
-                        <Link to='/home' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 hover:animate-bounce-slow mx-16">
+                        <Link to='/home' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 hover:animate-bounce-slow mx-8">
                             Home
                         </Link>
-                        <Link to='/courses' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 hover:animate-bounce-slow mx-16">
+                        <Link to='/courses' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 hover:animate-bounce-slow mx-8">
                             Courses
                         </Link>
-                        <Link to='/instructors' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 hover:animate-bounce-slow mx-16">
+                        <Link to='/instructors' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 hover:animate-bounce-slow mx-8">
                             Instructors
                         </Link>
-                        <Link to='/aboutus' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white hover:animate-bounce-slow mx-16">
+                        <Link to='/aboutus' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white hover:animate-bounce-slow mx-8">
                             About Us
                         </Link>
                     </div>
